1f8624148e9ba3bfa202d436e142deb3ceb6a67f
DESKTOP-KAY\kay <kaythechemist@gmail.com>
2019-01-02 23:42:02 +0900

    2019-01-02 main 메소드에 추가

--- src/algorithm_study/NoSameNumber.java
+++ src/algorithm_study/NoSameNumber.java
@@ -19,6 +19,27 @@ import java.util.*;
  */
 public class NoSameNumber {
 	
+	public static void main(String[] args) {
+		int[] arr = {1, 1, 3, 3, 0, 1, 1};
+		
+		List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] != arr[i]) {
+                list.add(arr[i]);
+            }
+        }
+        
+        int[] answer = new int[list.size()];
+        
+        for (int j = 0; j < list.size(); j++) {
+            answer[j] = list.get(j);
+        }
+        
+        System.out.println(list.toString());
+	}
+	
 	public int[] solution(int []arr) {
         List<Integer> list = new ArrayList<>();
         list.add(arr[0]);
