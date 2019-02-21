407112afec0e017674f059ecc6f0e25297389eb0
DESKTOP-KAY\kay <kaythechemist@gmail.com>
2019-02-21 15:41:11 +0900

    2019-02-21 수박수박수박수박수박수박수박수박수박수박수박수박수

---
+++ src/algorithm_study/Watermelon.java
@@ -0,0 +1,41 @@
+package algorithm_study;
+
+/*
+ * 길이가 n이고, 수박수박수박수....와 같은 패턴을 유지하는 문자열을 리턴하는 함수, solution을 완성하세요.
+ * 예를들어 n이 4이면 수박수박을 리턴하고 3이라면 수박수를 리턴하면 됩니다.
+
+	제한 조건
+	n은 길이 10,000이하인 자연수입니다.
+	
+	--파이썬 풀이--
+	
+	def solution(n):
+    answer = ''
+    for i in range(1, n+1):
+        if i % 2 == 1:
+            answer = answer + "수"
+        else:
+            answer = answer + "박"
+    return answer
+ */
+public class Watermelon {
+	public String solution(int n) {
+	      String answer = "";
+	      for (int i = 1; i <= n; i++) {
+	          if (i % 2 == 1) {
+	              answer = answer + "수";
+	          } else {
+	              answer = answer + "박";
+	          }
+	      }
+	      return answer;
+	  }
+	
+	public static void main(String[] args) {
+		Watermelon wm = new Watermelon();
+		String oddMelon = wm.solution(3);
+		String evenMelon = wm.solution(4);
+		System.out.println(oddMelon);
+		System.out.println(evenMelon);
+	}
+}
